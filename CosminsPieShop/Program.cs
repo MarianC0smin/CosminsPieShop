28bcad19f77a9d4256475f827da8a02fee4a03d7
Cosmin <mariancosmin30@yahoo.com>
2023-01-10 18:42:19 +0200

    register new services using Dependency Injection Container

--- CosminsPieShop/Program.cs
+++ CosminsPieShop/Program.cs
@@ -1,4 +1,10 @@
+using CosminsPieShop;
+using CosminsPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
