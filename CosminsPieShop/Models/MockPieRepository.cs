4cb1869e87e83d409086757a033df0dbaa10a395
Cosmin <mariancosmin30@yahoo.com>
2023-01-10 18:36:15 +0200

    Added MockPieRepository that implements IPieRepository and provide hardcoded data

---
+++ CosminsPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace CosminsPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie { PieId = 1, Name = "Strawberry Pie", Price = 15.95M, IsPieOfTheWeek = true}
+            };
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return AllPies.Where(p => p.IsPieOfTheWeek);
+            }
+        }
+        Pie? IPieRepository.GetPieById(int pieId) => AllPies.FirstOrDefault(p => p.PieId == pieId);
+    }
+}
