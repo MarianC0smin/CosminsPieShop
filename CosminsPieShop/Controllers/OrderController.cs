baa76bfb5db9b9b0cb714cee9198a4c0730e05c4
Cosmin <mariancosmin30@yahoo.com>
2023-02-07 20:41:36 +0200

    Added Authorization

--- CosminsPieShop/Controllers/OrderController.cs
+++ CosminsPieShop/Controllers/OrderController.cs
@@ -1,8 +1,10 @@
 ﻿using CosminsPieShop.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CosminsPieShop.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IOrderRepository _orderRepository;
