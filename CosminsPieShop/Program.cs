8d962958b7c46884ba183910789cc9042162b604
Cosmin <mariancosmin30@yahoo.com>
2023-02-07 20:36:16 +0200

    Added Authentification using Identity

--- CosminsPieShop/Program.cs
+++ CosminsPieShop/Program.cs
@@ -2,8 +2,10 @@ using CosminsPieShop.Models;
 using Microsoft.EntityFrameworkCore;
 using  Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("CosminsPieShopDbContextConnection") ?? throw new InvalidOperationException("Connection string 'CosminsPieShopDbContextConnection' not found.");
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
@@ -24,6 +26,9 @@ builder.Services.AddDbContext<CosminsPieShopDbContext>(options => {
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:CosminsPieShopDbContextConnection"]);
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<CosminsPieShopDbContext>();
 
 builder.Services.AddServerSideBlazor();
 
